45bf8bac45ab235153f305952038f3a257f36e2d
iiakovlev <Iaroslav.Iakovlev@best-gaming.com>
2016-11-03 18:25:39 +0100

    mod: matching strategy stuff

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,13 +9,15 @@ import {NotFoundComponent} from "./param/not-found.component";
 
 let urls = {
   empty: "", 
+  home: "home",
   about: "about",
   param: "param-is/:parent",
   child: "i-am-child"
 };
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
+  { path: "", redirectTo: "/home", pathMatch: "full" },  
+  { path: "home", component: HomeComponent},
   { path: "page-not-found", component: NotFoundComponent },
   { path: urls.about, component: AboutComponent },
   { path: "param-is/specific-route", component: SpecificComponent},
@@ -24,8 +26,7 @@ const routes: Routes = [
       { path: "", component: ChildComponent },
       { path: urls.child, component: ChildComponent }
     ]
-  },
-  { path: "**", redirectTo: "/page-not-found" }    
+  }    
 ];
 
 export const routing = RouterModule.forRoot(routes);
--- src/readme.md
+++ src/readme.md
@@ -1,16 +1,14 @@
 Angular2 routing
 ================
 
-Step: s1e4-url-matching
+Step: s1e7-url-matching-misc
 -------------
 
-Currently we have 3 routes and 3 corresponding components:
-**HomeComponent**, **AboutComponent** and **ParamComponent**
-Lets check how urls are matched by the router.
-1. The exact url is matched if its specified by the route.
-2. if you specify a parameter, then you should start with a colon symbol ":", like :anything
-3. whatever you put in the place of a placeholder will match, except for the empty string
-4. routes are interpreted in the order of appearance, specificity doesnt matter.
+We maybe missed some things about routes matching.
+How to set the route by default?
+What are the different matching strategies?
+
+prefix vs full matching strategies difference
 
 
 
